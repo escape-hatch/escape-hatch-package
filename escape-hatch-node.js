4593d2bfd01986ebab526164c0d46bdebc7502df
Josh White <whitejosh42@gmail.com>
2017-07-07 16:42:54 -0500

    feat(exit code): add process.exitCode to preserve exit status

--- escape-hatch-node.js
+++ escape-hatch-node.js
@@ -5,6 +5,7 @@ const escapeHatch = function () {
   process.on('uncaughtException', function (err) {
     console.error(err)
     console.log(chalk.bold.underline.gray.bgBlue(`Need help?  Try the Escape Hatch:\n-> https://escapehatch.herokuapp.com/links/${base64url.encode(`${err.name}: ${err.message}`)}`))
+    process.exitCode = 1
   })
 }
 
